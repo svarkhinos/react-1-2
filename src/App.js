179f0ccad85afbf5a16a89e30cecbb252a4f903e
svarkhinos <svitlana.arkhinos@gmail.com>
2021-10-20 10:31:48 +0300

    test2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import PaintingList from './components/paintingList';
+import Section from './components/section';
+import paintings from './painting.json';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Section title="The BEST">
+        <PaintingList items={paintings} />
+      </Section>
+      <Section />
     </div>
   );
 }
-
-export default App;
---
+++ src/components/Painting.js
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+import defaultImage from './default.jpg';
+
+export default function Painting({
+  imageUrl = defaultImage,
+  title,
+  profileUrl,
+  author = 'unknown',
+  price,
+  quantity,
+}) {
+  return (
+    <div>
+      <img src={imageUrl ?? defaultImage} alt={title} width="480" />
+      <h2>{title}</h2>
+      <p>
+        Author: <a href={profileUrl}>{author}</a>
+      </p>
+      <p>Price: {price}dollars</p>
+      <p>Avaliable: {quantity < 10 ? 'sorry, but NO' : 'Yes, you get it'}</p>
+      <button type="button">Add to bin</button>
+    </div>
+  );
+}
+
+Painting.propTypes = {
+  imageUrl: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  profileUrl: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  quantity: PropTypes.number.isRequired,
+};
